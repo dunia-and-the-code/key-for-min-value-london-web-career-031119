5fa925ddd5f8382dd883a73550796f3e8e67a2ed
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-03-12 21:28:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  smallest_value =""
+  smallest_key = nil
   name_hash.each do |key, value|
-    
+    smallest_key = 
    end
 end
 
