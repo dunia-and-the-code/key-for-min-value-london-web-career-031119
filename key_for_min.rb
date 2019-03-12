080264089336a23243321048f918d5e3dd7ea155
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-03-12 21:30:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -16,14 +16,3 @@ end
 
 
 
-def key_for_min_value(hash)
-  lowest_key = nil	  lowest_key = nil
-  lowest_value = Float::INFINITY	  lowest_value = nil
-  hash.each do |k, v|	  hash.each do |k, v|
-    if v < lowest_value	    if lowest_value == nil || v < lowest_value
-      lowest_value = v	      lowest_value = v
-      lowest_key = k	      lowest_key = k
-    end	    end
-  end	  end
-  lowest_key	  lowest_key
-end 	end
