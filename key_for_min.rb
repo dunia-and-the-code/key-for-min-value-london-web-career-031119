52067d03067fa73a72e264a8dc23ad4f76d42503
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-03-12 21:32:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   smallest_key = nil 
   smallest_value = nil
   name_hash.each do |key, value|
-    if smallest_key > 
+    if smallest_key == nil || 
     smallest_key = 
    end
 end
